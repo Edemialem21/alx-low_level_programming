1aa8284bbbf89eb429ab628e9d6f352fd1726469
Edemialem21 <21edemialem@gmail.com>
2023-07-13 09:38:57 -0700

    First c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /*
  * main - print the text
+ * it is a function which prints 
  * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
