b4169315f4b9c2f818aac20a3743fc856c49ce91
Edemialem21 <21edemialem@gmail.com>
2023-07-14 12:21:46 -0700

    Three digits combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print three  digits  with comma and space
+ *
+ * Return: return 0 success
+ */
+
+int main(void)
+{
+	int x = 48;
+	int y = 49;
+	int z = 50;
+
+	while (x < 56)
+	{
+		y = x + 1;
+		while (y < 57)
+		{
+			z = x + 2;
+			while (z < 58)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(x);
+				if (!((x == 55) && (y == 56) && (z == 57)))
+				{
+					putchar(44);
+					putchar(32);
+				}
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
