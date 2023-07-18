6f7a79280695a626f26c2f0213acdac51409102f
Edemialem21 <21edemialem@gmail.com>
2023-07-18 12:59:25 -0700

    adding two numbrs

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integres
+ *
+ * @i: first parameter
+ * @j: the second operand to be added
+ * Return: Always 0 (Succeed)
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
