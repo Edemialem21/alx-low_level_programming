5aa83475b7094340794e8ce83338d37a88e3173e
Edemialem21 <21edemialem@gmail.com>
2023-07-20 11:56:52 -0700

    printing triangle of hashh

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,7 +12,7 @@ void print_triangle(int size)
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j < size - i; j++)
+			for (j = 1; j <= size - i; j++)
 				_putchar(32);
 			for (x = 1; x <= i; x++)
 				_putchar(35);
