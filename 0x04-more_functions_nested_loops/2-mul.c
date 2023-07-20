5e4d0fed8e3ab676527f69d5a67e6fa6b260b9f5
Edemialem21 <21edemialem@gmail.com>
2023-07-20 08:44:03 -0700

    integer multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - iteger multiplicaion
+ *
+ * @a: the first integer
+ * @b: the second integer
+ * Return: return the result
+ */
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
