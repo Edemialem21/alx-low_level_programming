95a80eca9c630642eb38f63f86585d172560ac69
Edemialem21 <21edemialem@gmail.com>
2023-07-27 10:57:59 -0700

    lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,19 @@
 #include "main.h"
 /**
+ * string_toupper - lowercase to uppercase
+ * @str: the given string
+ * Return: Always0
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
