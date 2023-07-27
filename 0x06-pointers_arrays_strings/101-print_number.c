b04ee1ec1b1074185d1a49cc03b01d205e9984f0
Edemialem21 <21edemialem@gmail.com>
2023-07-27 11:42:31 -0700

    printng integer numbers recursevily

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_number - print integer numbers
+  * @n: input integer to be printed
+  * Return: always 0 success
+  */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
