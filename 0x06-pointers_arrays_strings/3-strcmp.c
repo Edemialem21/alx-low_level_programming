69395ccd3b9df5fe3fa6ba2e158b26e922bd4622
Edemialem21 <21edemialem@gmail.com>
2023-07-27 10:08:13 -0700

    string comparation

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,11 @@
 int _strcmp(char *s1, char *s2)
 {
 	int a = 0;
-	int b = 0;
 
-	while (s1[a] != '\0' && s2[a] != '\0')
+	while (s1[a] != '\0' || s2[a] != '\0')
 	{
-		b = s1[a] - s2[a];
-		if (b < 0)
-			return (-15);
-		if (b > 0)
-			return (15);
+		if (s1[a] != s2[a])
+		       return (s1[a] - s2[a]);
 		a++;
 	}
 		return (0);
