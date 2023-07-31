fb7979d62950c1574a3d019d0b0129039a958733
Edemialem21 <21edemialem@gmail.com>
2023-07-31 08:05:36 -0700

    memrory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory
+ * @dest: the destination pointer
+ * @src: the source where in the copy taken
+ * @n: the number of byte to be copyed
+ * Return: retuurn the pointer containing the resultant
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (n > 0 && src[i] != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+		n--;
+	}
+	return (dest);
+}
+
