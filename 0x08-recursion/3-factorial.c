fdd77b417f7cc98e0ba5efcc731a56673cbfe1e5
Edemialem21 <21edemialem@gmail.com>
2023-08-02 08:44:28 -0700

    factorial of a number recurssively

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,5 +1,14 @@
 #include "main.h"
 /**
- * factorial - 
+ * factorial - calculate factorial of the number
+ * @n: the input number
+ * Return: the factoral of @n
  */
 int factorial(int n)
+{
+	if (n == 1)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
