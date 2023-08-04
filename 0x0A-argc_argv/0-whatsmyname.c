aeb8437835899a5d3948384d77b3d2edd1fbed56
Edemialem21 <21edemialem@gmail.com>
2023-08-04 11:39:57 -0700

    argument vector and argument total

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the entry function
+ * @argc: the number of arguement
+ * @argv: the vector containing the input parameter
+ * Return: always 0 for success
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return 0;
+}
