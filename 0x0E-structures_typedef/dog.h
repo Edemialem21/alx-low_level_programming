5d14eaaf1f278f11c05d9a8fb9bff035173ec0cc
Edemialem21 <21edemialem@gmail.com>
2023-08-14 10:46:20 -0700

    structure defination

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - theh dog attributes
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the this dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-struct dog my_dog;
+typedef struct dog my_dog;
+#endif
