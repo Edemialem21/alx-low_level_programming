3193001c671388ca16bfbe677e99df2e5507a4cc
Edemialem21 <21edemialem@gmail.com>
2023-08-17 05:54:50 -0700

    print string using vardic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,22 +9,22 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-        va_list args;
-        unsigned int i;
-	char* str;
+	va_list args;
+	unsigned int i;
+	char *str;
 
-        va_start(args, n);
+	va_start(args, n);
 
-        for (i = 0; i < n; i++)
-        {
+	for (i = 0; i < n; i++)
+	{
 		str = va_arg(args, char*);
 		if (str == NULL)
 			printf("nil");
 		else
 			printf("%s", str);
-                if (separator && i < n - 1)
-                        printf("%s", separator);
-        }
-        printf("\n");
-        va_end(args);
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
 }
