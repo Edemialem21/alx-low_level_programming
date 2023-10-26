0daeb357d29c8f98fd2118ee34b16efa51b6d1e6
Edemialem21 <21edemialem@gmail.com>
2023-10-26 10:53:29 -0700

    Bit manipulation of binary to integer conversion

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include <string.h>
+/*
+ * binary_to_uint - binary to integer conversion
+ * @b: pointer to the binarys
+ * Return: an integer or null
+ */ 
+
+unsigned int binary_to_uint(const char *b)
+{
+	if (b == NULL)
+		return 0;
+	unsigned int result = 0;
+	int len = strlen(b);
+
+	for (int i = 0; i < len; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return 0;
+		result = (result << 1) + (b[i] - '0');
+	}
+	return result;
+}
