78ee8d9525555081d29d1e452fef164af33919fc
Edemialem21 <21edemialem@gmail.com>
2023-11-03 11:55:14 -0700

    the fiele descriprator

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * print_error - the function that print error
- * @message: a pointer to memorry
+ * @file_des: a pointer to memorry
  * Return: returns the letter for success
  */
-void print_error(const char *message)
+void print_error(int file_des)
 {
-	dprintf(STDERR_FILENO, "Error: %s\n", message);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_des);
+	exit(100);
 }
 /**
  * main - the function that print error
@@ -16,44 +17,44 @@ void print_error(const char *message)
  */
 int main(int argc, char *argv[])
 {
-	char *file_from = argv[1], *file_to = argv[2];
-	int fd_from = open(file_from, O_RDONLY);
-	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
+	char buffer[1024];
+	int file_from = open(argv[1], O_RDONLY), wc;
+	int file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	if (argc != 3)
 	{
-		print_error("Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (fd_from == -1 || fd_to == -1)
+	if (argv[1] == NULL || file_from < 0)
 	{
-		print_error(fd_from == -1 ? "Can't read from file" : "Can't write to file");
-		print_error(fd_from == -1 ? file_from : file_to);
-		exit(fd_from == -1 ? 98 : 99);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	if (file_to < 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(file_from);
+		exit(99);
+	}
+	while ((wc = read(file_from, buffer, 1024)) > 0)
+	{
+		if (wc != write(file_to, buffer, wc))
 		{
-			print_error("Can't write to file");
-			print_error(file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(file_from);
+			close(file_to);
 			exit(99);
 		}
 	}
-	if (bytes_read == -1)
+	if (wc < 0)
 	{
-		print_error("Can't read from file");
-		print_error(file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-	{
-		print_error("Can't close fd");
-		dprintf(STDERR_FILENO, "%d\n", fd_from);
-		exit(100);
-	}
+	if (close(file_from) < 0)
+		print_error(file_from);
+	if (close(file_to) < 0)
+		print_error(file_to);
 	return (0);
 }
