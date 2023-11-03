445b312fb6f0baaf2bdef347c92103aa1122c160
Edemialem21 <21edemialem@gmail.com>
2023-11-03 04:55:06 -0700

    the function that write to file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - the function that creat text file
+ * @filename: a pointer to memorry
+ * @text_content: the parameter that specifiy the number of letters to be write
+ * Return: returns the letter for success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	if (filename == NULL)
+		return (-1);
+
+	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
