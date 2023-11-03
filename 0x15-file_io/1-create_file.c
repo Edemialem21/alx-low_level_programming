774016a85371b6f0022faebfddbabf3fa65440f2
Edemialem21 <21edemialem@gmail.com>
2023-11-03 05:07:26 -0700

    the function that write to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,18 +9,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+	
 	if (filename == NULL)
 		return (-1);
-
-	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-
 	if (fd == -1)
 		return (-1);
-
 	if (text_content != NULL)
 	{
-		ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-
 		if (bytes_written == -1)
 		{
 			close(fd);
