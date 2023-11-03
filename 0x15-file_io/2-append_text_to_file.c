ef8ea70d800ae49d82ce5411d7c2491dd2b7a3e9
Edemialem21 <21edemialem@gmail.com>
2023-11-03 18:54:20 +0300

    file append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - the function that append the file
+ * @filename: a pointer to memorry
+ * @text_content: the parameter that specifiy the number of letters to be read
+ * Return: returns the letter for success
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int fd = open(filename, O_WRONLY | O_APPEND);
+
+    if (filename == NULL)
+        return (-1);
+    if (fd == -1)
+        return (-1);
+    if (text_content != NULL)
+    {
+        ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+        if (bytes_written == -1)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
+    close(fd);
+    return (1);
+}
