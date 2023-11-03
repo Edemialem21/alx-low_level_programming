32fa50394310e31229ffd5f7216203536b692665
Edemialem21 <21edemialem@gmail.com>
2023-11-03 12:13:09 -0700

    that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,10 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
