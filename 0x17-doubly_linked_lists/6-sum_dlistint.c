1d03b94c02a51d613c2ca64226228a9e2c9e0d7d
Edemialem <21edemialem@gmail>
2023-12-08 15:43:24 +0300

    returns the sum of the nodes data

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * sum_dlistint - returns the sum of all the doubly linked list nodes data
+ * @head: head pointer to the doubly liinked list
+ * Return: it returns the sum of the nodes data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr;
+	int sum = 0;
+
+	ptr = head;
+	if (head == NULL)
+		return (0);
+	while (ptr != NULL)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+        }
+        return (sum);
+}
+
