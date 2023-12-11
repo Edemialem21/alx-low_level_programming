15dad6c9470b2b9d3d7b442c57e96cea805ea170
Edemialem <21edemialem@gmail>
2023-12-11 19:00:34 +0300

    dynamic liberaries of c shred for python

---
+++ 0x18-dynamic_libraries/operation.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers together
+ * @a: first input integer to multiply
+ * @b: second input integer to multiply
+ * * Return: integer product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * add - add two integers together
+ * @a: first input integer to multiply
+ * @b: second input integer to multiply
+ * * Return: integer addition of a and b
+ */
+int add(int a, int b)
+{
+        return (a + b);
+}
+
+/**
+ * sub - subtract two integers together
+ * @a: first input integer to multiply
+ * @b: second input integer to multiply
+ * * Return: integer difference of a and b
+ */
+int sub(int a, int b)
+{
+        return (a -  b);
+}
+
+/**
+ * div - divide two integers together
+ * @a: first input integer to multiply
+ * @b: second input integer to multiply
+ * * Return: integer addition of a and b
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - modules two integers together
+ * @a: first input integer to multiply
+ * @b: second input integer to multiply
+ * * Return: integer modules of a and b
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
