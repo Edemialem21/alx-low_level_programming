78bf7bb280ead17e52e77d692b5da48473a97cef
Edemialem <21edemialem@gmail>
2023-12-13 10:56:25 +0300

    congratulatio to win the jackbow

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.githubusercontent.com/Edemialem21/alx-low_level_programming/master/0x18-dynamic_libraries/libage.so
-export LD_PRELOAD=/tmp/libage.so
+wget -P .. https://raw.githubusercontent.com/Edemialem21/alx-low_level_programming/master/0x18-dynamic_libraries/libagee.so
+export LD_PRELOAD="$PWD/../libagee.so"
