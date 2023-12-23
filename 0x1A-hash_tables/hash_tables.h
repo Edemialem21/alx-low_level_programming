036a2c11df82d2ffa74af23b3aac3720d254769a
Edemialem21 <21edemialem@gmail.com>
2023-12-23 09:15:31 +0300

    hash table shash

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SUCCESS 1
+#define FAILURE 0
+
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -93,5 +97,7 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
+
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 
 #endif
