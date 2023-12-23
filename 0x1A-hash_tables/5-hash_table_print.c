9383d1e6fd3b6bcd6c7975750aba87d95025858d
Edemialem21 <21edemialem@gmail.com>
2023-12-23 09:10:10 +0300

    hash table get

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: a pointer that points to hash table.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *traverse;
+	unsigned long int i = 0, step = 1;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (i < ht->size)
+	{
+		traverse = ht->array[i];
+		while (traverse)
+		{
+			if (step)
+			{
+				printf("'%s': '%s'", traverse->key, traverse->value);
+				step = 0;
+			}
+			else
+				printf(", '%s': '%s'", traverse->key, traverse->value);
+			traverse = traverse->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}
